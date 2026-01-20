00604234f066269345e6b714fb198705e9c3d9b5
Kanak Bhardwaj <kanakbhardwaj823@gmail.com>
2026-01-20 11:50:03 +0530

    Initial commit

---
+++ src/main/java/com/example/day3_lms/Day3LmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3_lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3LmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3LmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day3_lms/Day3LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3_lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3LmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
